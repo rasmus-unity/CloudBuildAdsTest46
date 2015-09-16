20feb218b4ee558d14dc1dfcac67426acd03dd6f
Rasmus Selsmark <rasmuss@Rasmuss-MacBook-Pro-2.local>
2015-09-17 00:10:11 +0300

    First version

---
+++ Assets/Main.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class Main : MonoBehaviour
+{
+	void Start ()
+	{
+		Advertisement.Initialize("14851");
+	}
+
+	public void ShowAd()
+	{
+		if (!Advertisement.IsReady ())
+		{
+			Debug.Log("Ads not ready");
+			return;
+		}
+
+		Advertisement.Show ();
+	}
+}
